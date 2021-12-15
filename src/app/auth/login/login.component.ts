ffa74cd210046dbb0baecfa62663a922381c8dc5
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-12-15 18:10:14 -0500

    s02-08 The Store Service API - Implementing the Login Screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import {Store} from "@ngrx/store";
+import {Store} from '@ngrx/store';
 
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import {AuthService} from '../auth.service';
+import {tap} from 'rxjs/operators';
+import {noop} from 'rxjs';
+import {Router} from '@angular/router';
+import {AppState} from '../../reducers';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
@@ -18,9 +20,10 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-      private fb:FormBuilder,
+      private fb: FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router: Router,
+      private store: Store<AppState>) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -35,6 +38,22 @@ export class LoginComponent implements OnInit {
 
   login() {
 
+      const val = this.form.value;
+      this.auth.login(val.email, val.password)
+          .pipe(
+              tap(user => {
+
+                  console.log(`Login: ${JSON.stringify(user)}`);
+
+                  // this.store.dispatch();
+
+                  this.router.navigateByUrl('/courses');
+              })
+          )
+          .subscribe(
+              noop,
+              () => alert('Login Failed')
+          );
   }
 
 }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,12 +1,8 @@
 import {
-  ActionReducer,
   ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import {routerReducer} from '@ngrx/router-store';
 
 // tslint:disable-next-line:no-empty-interface
 export interface AppState {
@@ -14,22 +10,9 @@ export interface AppState {
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  router: routerReducer
+
 };
 
-export function logger(reducer: ActionReducer<any>)
-  : ActionReducer<any> {
-  return (state, action) => {
-    console.log('state before: ', state);
-    console.log('action', action);
-
-    return reducer(state, action);
-  }
-
-}
-
-
-export const metaReducers: MetaReducer<AppState>[] =
-  !environment.production ? [logger] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
 
 
