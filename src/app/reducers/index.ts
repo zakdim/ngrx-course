527d6e417b1c62931408bfaaabee44ef64ece58e
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-12-25 16:59:25 -0500

    s04-24 NgRx Metareducers - Step-by-Step Implementation

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import {ActionReducer, ActionReducerMap, MetaReducer} from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { routerReducer } from '@ngrx/router-store';
 
@@ -9,6 +9,15 @@ export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer,
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production
-    ? []
-    : [];
+export function logger(reducer: ActionReducer<any>)
+    : ActionReducer<any> {
+    return (state, action) => {
+        console.log('state before: ', state);
+        console.log('action: ', action);
+
+        return reducer(state, action);
+    };
+}
+
+export const metaReducers: MetaReducer<AppState>[] =
+    !environment.production ? [logger] : [];
