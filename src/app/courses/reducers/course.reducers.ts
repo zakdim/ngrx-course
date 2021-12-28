149f784d8303c9e3b1293f29f1051a94afea403c
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-12-27 21:21:25 -0500

    s05-30 Implementing Reducers Using the NgRx Entity Adapter

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,7 +1,20 @@
-import {Course} from '../model/course';
-import {EntityState} from '@ngrx/entity';
+import { createEntityAdapter, EntityState } from "@ngrx/entity";
+import { createReducer, on } from "@ngrx/store";
+import { CourseActions } from "../action-types";
+import { Course } from "../model/course";
 
-export interface CoursesState extends EntityState<Course> {
-    // entities: {[key: number]: Course};
-    // ids: number[];
-}
+export const coursesFeatureKey = "courses";
+
+export interface CoursesState extends EntityState<Course> {}
+
+export const adapter = createEntityAdapter<Course>();
+
+export const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+    initialCoursesState,
+
+    on(CourseActions.allCoursesLoaded, (state, action) =>
+        adapter.setAll(action.courses, state)
+    )
+);
