d53be687c693ea03e88572277d5ad3b55a087fe2
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-01-01 19:15:05 -0500

    s05-35 Optimistic Data Editing - Reducer Implementation and Demo

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,7 +1,8 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
+
 import { CourseActions } from "../action-types";
-import {compareCourses, Course} from "../model/course";
+import { compareCourses, Course } from "../model/course";
 
 export const coursesFeatureKey = "courses";
 
@@ -14,19 +15,21 @@ export const adapter = createEntityAdapter<Course>({
 });
 
 export const initialCoursesState = adapter.getInitialState({
-    allCoursesLoaded: false
+    allCoursesLoaded: false,
 });
 
 export const coursesReducer = createReducer(
     initialCoursesState,
 
     on(CourseActions.allCoursesLoaded, (state, action) =>
-        adapter.setAll(
-            action.courses,
-            {
-                ...state,
-                allCoursesLoaded: true
-            }))
+        adapter.setAll(action.courses, {
+            ...state,
+            allCoursesLoaded: true,
+        })
+    ),
+
+    on(CourseActions.courseUpdated, (state, action) =>
+        adapter.updateOne(action.update, state) )
 );
 
 export const { selectAll } = adapter.getSelectors();
